d2332f4cb92bd5f3aa396f77c48d6969ff58730a
neeraj <nk88337@gmail.com>
2018-12-14 20:24:01 +0530

    initial

---
+++ app/src/main/java/org/study/chatmessagener/FirebaseMessage.java
@@ -0,0 +1,99 @@
+package org.study.chatmessagener;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessage extends FirebaseMessagingService {
+
+    public static final int DAILY_REMINDER_REQUEST_CODE=100;
+    public static final String NOTIFICATION_REPLY = "NotificationReply";
+    public static final String CHANNNEL_ID = "SimplifiedCodingChannel";
+    public static final String CHANNEL_NAME = "SimplifiedCodingChannel";
+    public static final String CHANNEL_DESC = "This is a channel for Simplified Coding Notifications";
+
+    public static final String KEY_INTENT_MORE = "keyintentmore";
+    public static final String KEY_INTENT_HELP = "keyintenthelp";
+
+    public static final int REQUEST_CODE_MORE = 100;
+    public static final int REQUEST_CODE_HELP = 101;
+    public static final int NOTIFICATION_ID = 200;
+    private final String TAG = FirebaseMessage.class.getSimpleName();
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d(TAG,"token "+s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom()+" collapse key "+remoteMessage.getCollapseKey());
+        if (!remoteMessage.getData().isEmpty()){
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null){
+            Log.d(TAG,"notification "+remoteMessage.getNotification().getBody());
+        }
+
+
+        //showNotification(getApplicationContext(),HomeActivity.class,remoteMessage.getNotification().getTitle()+" "+remoteMessage.toString(),"");
+    }
+
+
+    public static void showNotification(Context context, Class<?> cls, String title, String content)
+
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNNEL_ID, CHANNEL_NAME, importance);
+            mChannel.setDescription(CHANNEL_DESC);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Intent notificationIntent = new Intent(context, cls);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(cls);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(DAILY_REMINDER_REQUEST_CODE, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNNEL_ID);
+
+        Notification notification = mBuilder.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .setSound(alarmSound)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(DAILY_REMINDER_REQUEST_CODE, notification);
+
+    }
+}
---
+++ app/src/main/java/org/study/chatmessagener/HomeActivity.java
@@ -0,0 +1,35 @@
+package org.study.chatmessagener;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()){
+                        Log.e(TAG,"token not found");
+                    }else{
+                        String token = task.getResult().getToken();
+                        Log.d(TAG,"token "+token);
+                    }
+            }
+        });
+    }
+}
